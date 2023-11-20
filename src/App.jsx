550cc236751763d7e05373a4ea04a7b168373966
Mark Glasman <glasman.mark@gmail.com>
2023-11-20 15:21:12 -0500

    got puppy images and names to render in cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-
+import PuppyList from './components/PuppyList'
 import './App.css'
 
-function App() {
-  
 
+function App() {
+  const [puppies, setPuppies] = useState([]);
+  
   return (
     <>
       <p>Hello World</p>
+      <PuppyList puppies={puppies} setPuppies={setPuppies}/>
     </>
   )
 }
---
+++ src/components/PuppyList.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function PuppyList({ puppies, setPuppies }) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players"
+        );
+        const responseJSON = await response.json();
+        const puppyData = responseJSON.data.players;
+        setPuppies(puppyData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log({ puppies });
+
+  return (
+    <>
+      <h1>The Players</h1>
+
+      {puppies.map((pupper) => (
+        <div key={pupper.id}>
+          <p>Name: {pupper.name}</p>
+          <img src={pupper.imageUrl} />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default PuppyList;
